ad0a8d456babb8bd0b0c5f9c8aadc44b18e19bca
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-06-15 16:24:02 -0500

    logout function working

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
       <i class="material-icons" id="addButton">navigate_before</i>
     </md-button>
   </div>
-      <div>
+  <div>
     <h1>Log My Run</h1>
   </div>
       <div>
@@ -15,11 +15,18 @@
         <i class="material-icons" id="addButton">menu</i>
       </md-button>
       <md-menu-content width="2" ng-click="$mdMenu.close()">
+        <?php if( is_user_logged_in() && current_user_can( 'edit_posts' ) ) : ?>
         <md-menu-item>
-          <md-button ng-click="showRun.logout()"> <span md-menu-align-target>Logout</span></md-button>
+          <md-button><span md-menu-align-target><?php wp_loginout( $_SERVER['REQUEST_URI']); ?></span></md-button>
         </md-menu-item>
+      <?php else : ?>
+        <md-menu-item>
+          <md-button><span md-menu-align-target>Logout</span></md-button>
+        </md-menu-item>
+      <?php endif ?>
     </md-menu>
   </div>
+
   </div>
   </md-toolbar>
 </header>
