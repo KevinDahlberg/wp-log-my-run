16c300ea0c686362d5f6c5089bb00bd005cbff57
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-06-20 03:49:05 -0500

    saving changes

--- scripts/services/RunService.js
+++ scripts/services/RunService.js
@@ -18,23 +18,15 @@ myApp.factory('RunService', ['$http', '$location',
      */
     let dropdownTime = new Time(TIME);
     let dropdownMiles = new Distance(DISTANCE);
+
     let newRun = new Run(DEFAULT_RUN);
-    console.log('newRun ', newRun);
 
     /**
-     * @function enter view
-     * @desc changes the view to the enter_run view
+     * @function getRun
+     * @desc gets runs by a particular user from the DB
+     * @param user_ID
+     * @return all runs by that user
      */
-    let enterView = () => {
-      $location.path('/enter-run');
-    };
-
-    /**
-    * @function getRun
-    * @desc gets runs by a particular user from the DB
-    * @param user_ID
-    * @return all runs by that user
-    */
     let getRun = (user_ID) => {
       $http.get(WPsettings.root + 'wp/v2/posts?filter[author]=' + user_ID).then((response) => {
         console.log('in the get route is ', response);
@@ -44,18 +36,61 @@ myApp.factory('RunService', ['$http', '$location',
     getRun(WPsettings.user_ID);
 
     /**
-    * @function addRun
-    * @desc adds a run to the Database
-    * @param run from enter view
-    * @return response is the object that was sent.
-    */
+     * @function addRun
+     * @desc adds a run to the Database
+     * @param run from enter view
+     * @return response is the object that was sent.
+     */
     let addRun = (run) => {
-      let runData = new RunToSend (run);
-      let newRunReq = new PostRequest (runData.objectToSend);
+      let runData = new RunToSend(run);
+      let newRunReq = new PostRequest(runData.objectToSend);
 
       $http(newRunReq).then((response) => {
         console.log('post was successful, ', response);
       });
+    };
+
+    /**
+    * @function editRun
+    * @desc edits a run in the DB
+    * @param
+    * @return
+    */
+
+    /**
+    * @function deleteRun
+    * @desc deletes a run from the DB
+    * @param
+    * @return
+    */
+
+    /**
+    * @function addUser
+    * @desc adds user to the DB
+    * @param
+    * @return
+    */
+
+    /**
+    * @function editUser
+    * @desc edits user in the DB
+    * @param
+    * @return
+    */
+
+    /**
+    * @function deleteUser
+    * @desc deletes user from the DB
+    * @param
+    * @return
+    */
+
+    /**
+     * @function enter view
+     * @desc changes the view to the enter_run view
+     */
+    let enterView = () => {
+      $location.path('/enter-run');
     };
 
     return {
